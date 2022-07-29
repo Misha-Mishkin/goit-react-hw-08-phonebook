398d605789d15c5de3f9451cebe4a8fe58cf962e
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-07-30 00:01:47 +0300

    commit

--- src/redux/authAPI/auth-operation.js
+++ src/redux/authAPI/auth-operation.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const token = {
   set(token) {
@@ -40,40 +40,31 @@ const logOut = createAsyncThunk('auth/logout', async () => {
     // TODO: Добавить обработку ошибки error.message
   }
 });
-// /*
-//  * GET @ /users/current
-//  * headers:
-//  *    Authorization: Bearer token
-//  *
-//  * 1. Забираем токен из стейта через getState()
-//  * 2. Если токена нет, выходим не выполняя никаких операций
-//  * 3. Если токен есть, добавляет его в HTTP-заголовок и выполянем операцию
-//  */
-// const fetchCurrentUser = createAsyncThunk(
-//   'auth/refresh',
-//   async (_, thunkAPI) => {
-//     const state = thunkAPI.getState();
-//     const persistedToken = state.auth.token;
 
-//     if (persistedToken === null) {
-//       console.log('Токена нет, уходим из fetchCurrentUser');
-//       return thunkAPI.rejectWithValue();
-//     }
+const fetchCurrentUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
 
-//     token.set(persistedToken);
-//     try {
-//       const { data } = await axios.get('/users/current');
-//       return data;
-//     } catch (error) {
-//       // TODO: Добавить обработку ошибки error.message
-//     }
-//   },
-// );
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue();
+    }
+
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get('/users/current');
+      return data;
+    } catch (error) {
+      // TODO: Добавить обработку ошибки error.message
+    }
+  }
+);
 
 const operations = {
   register,
   logOut,
   logIn,
-  //   fetchCurrentUser,
+  fetchCurrentUser,
 };
 export default operations;
